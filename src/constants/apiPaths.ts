420e56d54340237468b0090c8d4086b6f2d3bf8e
fepu08 <ferencbesenyei@protonmail.com>
2023-10-25 20:42:26 +0200

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
+  import: "https://cfpa3haqo0.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
 };
 
 export default API_PATHS;
