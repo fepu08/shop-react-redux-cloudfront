9c65a041d72481fe3bdb992adf27f6c19218d84c
fepu08 <ferencbesenyei@protonmail.com>
2024-10-21 20:44:32 +0200

    updated frontend to use azure function app for serving products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,9 @@
 const API_PATHS = {
-  product: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
+  product:
+    "https://fa-products-service-sand-ne-008.azurewebsites.net/api/products",
   order: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
   import: "https://cfpa3haqo0.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
+  bff: "https://fa-products-service-sand-ne-008.azurewebsites.net/api",
   cart: "https://0trvutezp4.execute-api.eu-central-1.amazonaws.com",
 };
 
